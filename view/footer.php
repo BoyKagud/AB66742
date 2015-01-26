dfe9b61ea98dc9bb9809157e6dea0b6c7649bd7a
Mong-Teri <ricardo.emong@gmail.com>
2015-01-26 23:56:31 +0800

    aligned footer links to body wrap

--- view/footer.php
+++ view/footer.php
@@ -5,17 +5,17 @@
 <!-- Calculator Script -->
 <script src="<?php echo SCRIPTS_DIRECTORY; ?>/calculator.js" id="calculator-script"></script>
 
-<div class="footer">
-	<ul class="foot-links">
-		<li class="wleft"><a target="_blank" href="http://www.slicingpie.com/privacy-policy/">Privacy Policy</a><span class="margleft">|</span></li> 
-		<li class="wleft margleft"><a target="_blank" href="http://www.slicingpie.com/terms-of-service/">Terms of Service</a><span class="margleft">|</span></li>
-		<li class="wleft margleft"><a onclick="showCredits();" class="clickable">Credits</a></li> 
-	</ul>
+	<div class="footer">
+		<ul class="foot-links">
+			<li class="wleft"><a target="_blank" href="http://www.slicingpie.com/privacy-policy/">Privacy Policy</a><span class="margleft">|</span></li> 
+			<li class="wleft margleft"><a target="_blank" href="http://www.slicingpie.com/terms-of-service/">Terms of Service</a><span class="margleft">|</span></li>
+			<li class="wleft margleft"><a onclick="showCredits();" class="clickable">Credits</a></li> 
+		</ul>
 
-	<div class="wright margleft foot-feedback">
-		<a target="_blank" href="http://form.jotformpro.com/form/41825700042949">Send Feedback!</a>
+		<div class="wright margleft foot-feedback">
+			<a target="_blank" href="http://form.jotformpro.com/form/41825700042949">Send Feedback!</a>
+		</div>
 	</div>
-</div>
 </div>
 </div> <!-- Body Wrap -->
 
